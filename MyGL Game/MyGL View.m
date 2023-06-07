2412bbd643365d4fcea6588e67b649b60b3a1bac
yunagi490 <a23.2549tan@gmail.com>
2023-06-07 17:52:27 +0900

    commit

--- MyGL Game/MyGL View.m
+++ MyGL Game/MyGL View.m
@@ -1,18 +1,30 @@
-//
-//  MyGL View.m
-//  MyGL Game
-//
-//  Created by 夕凪時雨 on 2023/05/31.
-//
-
 #import "MyGL View.h"
+#import <OpenGL/OpenGL.h>
+#import <OpenGL/gl3.h>
 
-@implementation MyGL_View
+@implementation MyGLView
 
-- (void)drawRect:(NSRect)dirtyRect {
-    [super drawRect:dirtyRect];
+-(instancetype)initWithFrame:(NSRect)frame
+{
+    NSOpenGLPixelFormatAttribute attes[]={
+        NSOpenGLPFAAllowOfflineRenderers,
+        NSOpenGLPFAAccelerated,
+        NSOpenGLPFADoubleBuffer,
+        NSOpenGLPFAColorSize,32,
+        NSOpenGLPFADepthSize,32,
+        NSOpenGLPFAMultisample,
+        NSOpenGLPFASampleBuffers,1,
+        NSOpenGLPFASamples,4,
+        NSOpenGLPFANoRecovery,
+        NSOpenGLPFAOpenGLProfile, NSOpenGLPFAOpenGLProfileVersion3_2Core,0
+    };
+    NSOpenGLPixelFormat *pixelFormat=[[NSOpenGLPixelFormat alloc] initWithAttributes:attes];
     
-    // Drawing code here.
+    self=[super initWithFrame:frame pixelFormat:pixelFormat];
+    if(self){
+        
+    }
 }
-
 @end
+
+
--- MyGL Game/ViewController.m
+++ MyGL Game/ViewController.m
@@ -1,17 +1,40 @@
-//
-//  ViewController.m
-//  MyGL Game
-//
-//  Created by 夕凪時雨 on 2023/05/31.
-//
-
 #import "ViewController.h"
+#import "MyGL View.h"
 
 @implementation ViewController
+- (instancetype)initWithFrame:(NSRect)frame
+{
+    NSOpenGLPixelFormatAttribute attrs[] =
+    {
+        NSOpenGLPFAAllowOfflineRenderers,
+        NSOpenGLPFAAccelerated,
+        NSOpenGLPFADoubleBuffer,
+        NSOpenGLPFAColorSize, 32,
+        NSOpenGLPFADepthSize, 32,
+        NSOpenGLPFAMultisample,
+        NSOpenGLPFASampleBuffers, 1,
+        NSOpenGLPFASamples, 4,
+        NSOpenGLPFANoRecovery,
+        NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
+        0
+    };
+    NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] inutWithAttributes:attrs];
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+    self=[super initWithFrame:frame pixelFormat:pixelFormat];
+    if(self){
+        [self setWantsBestResolttionOpenGLSurface:YES];
+    }
+    return self;
 }
+
+-(void)prepareOpenGL
+{
+    [super prepareOpenGL];
+    NSOpenGLContext *glContext = [self openGLContext];
+
+    glClearColer(1.0f,0.0f,1.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    [glContext flushBuffer];
+}
+
 @end
